d06525787fe6c8e320724079205fac33ea03c2e1
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-07-16 01:44:10 +0300

    the storage local and the refactoring

--- src/feature/cart/cartSlice.js
+++ src/feature/cart/cartSlice.js
@@ -15,7 +15,20 @@ const cartSlice = createSlice({
   initialState: defaultState,
   reducers: {
     addItem: (state, action) => {
-      console.log(action);
+      const { product } = action.payload;
+      const item = state.cartItems.find((i = i.cardID === product.cardID));
+      if (item) {
+        item.amount += product.amount;
+      } else {
+        state.cartItems.push(product);
+      }
+
+      state.numItemsInCart += product.amount;
+      state.cartTotal += product.price * product.amount;
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      localStorage.setItem("cart", JSON.stringify(state));
+      toast.success("item added to cart");
     },
 
     claerCart: (state) => {},
