168aeee4435060f6b13389055097f48972f69801
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-07-16 09:41:38 +0300

    the next class will cart component

--- src/feature/cart/cartSlice.js
+++ src/feature/cart/cartSlice.js
@@ -10,13 +10,17 @@ const defaultState = {
   orderTotal: 0,
 };
 
+const getCartFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("cart") || defaultState);
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState: defaultState,
+  initialState: getCartFromLocalStorage(),
   reducers: {
     addItem: (state, action) => {
       const { product } = action.payload;
-      const item = state.cartItems.find((i = i.cardID === product.cardID));
+      const item = state.cartItems.find((i) => i.cardID === product.cardID);
       if (item) {
         item.amount += product.amount;
       } else {
@@ -25,15 +29,48 @@ const cartSlice = createSlice({
 
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
-      state.tax = 0.1 * state.cartTotal;
-      state.orderTotal = state.cartTotal + state.shipping + state.tax;
-      localStorage.setItem("cart", JSON.stringify(state));
+
+      cartSlice.caseReducers.calculateTotals(state);
+
       toast.success("item added to cart");
     },
 
-    claerCart: (state) => {},
-    removeItem: (state, action) => {},
-    editItem: (state, action) => {},
+    // ============= clear cart ==============
+
+    claerCart: (state) => {
+      localStorage.setItem("cart", JSON.stringify(defaultState));
+      return defaultState;
+    },
+
+    // ================= remove Item ==================
+
+    removeItem: (state, action) => {
+      const { cardID } = action.payload;
+      const product = state.cartItems.find((i) => i.cartID === cardID);
+      state.cartItems = state.cartItems.filter((i) => i.cardID !== cardID);
+
+      state.numItemsInCart -= product.amount;
+      state.cartTotal -= product.price * product.amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.error("Item removed from cart ");
+    },
+
+    // ================ Edit Item ================
+    editItem: (state, action) => {
+      const { cardID, amount } = action.payload;
+      const item = state.cartItems.find((i) => i.cardID === cardID);
+      state.numItemsInCart += amount - item.amount;
+      state.cartTotal += item.price * (amount - item.amount);
+      item.amount = amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.success("Cart updated");
+    },
+
+    calculateTotals: (state) => {
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
   },
 });
 
